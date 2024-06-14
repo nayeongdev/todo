e1eb8097e0c4709e85f0cf190b4fe61f613ecbc7
nayeongdev <nayeongkim.dev@gmail.com>
2024-06-14 14:01:54 +0900

    feat: ADD todo/task update and delete func, pagination, change localstorage to cookie

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,8 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'),
     path('todo-create/', views.TodoCreate.as_view(), name='todo_create'),
+    path('todo-update/<int:pk>', views.TodoUpdate.as_view(), name='todo_update'),
+    path('todo-delete/<int:pk>', views.TodoDelete.as_view(), name='todo_delete'),
+    path('task-update/<int:pk>', views.TaskUpdate.as_view(), name='task_update'),
+    path('task-delete/<int:pk>', views.TaskDelete.as_view(), name='task_delete'),
 ]
