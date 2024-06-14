1014d3d0613d7c1b516428326410bc1356a4af04
nayeongdev <nayeongkim.dev@gmail.com>
2024-06-14 14:01:54 +0900

    feat: ADD todo/task update and delete func, pagination, change localstorage to cookie

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
+from django.core.checks import messages
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from main.models import Todo, Task
 
@@ -16,8 +18,8 @@ def home(request):
     todo_paginator = Paginator(todo_list, 5)
     task_paginator = Paginator(task_list, 5)
 
-    todo_page_number = request.GET.get('todo_page', 1)
-    task_page_number = request.GET.get('task_page', 1)
+    todo_page_number = request.GET.get('todo', 1)
+    task_page_number = request.GET.get('task', 1)
 
     todos = todo_paginator.page(todo_page_number)
     tasks = task_paginator.page(task_page_number)
@@ -36,4 +38,42 @@ class TodoCreate(CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class TodoUpdate(UpdateView):
+    model = Todo
+    fields = ['category', 'title', 'done', 'priority', 'start_date', 'end_date', 'interval', 'description']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class TodoDelete(DeleteView):
+    model = Todo
+    context_object_name = 'todo'
+    success_url = reverse_lazy('main:home')
+
+    def form_valid(self, form):
+        messages.success(self.request, "The todo was deleted successfully.")
+        return super().form_valid(form)
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'done', 'priority', 'description']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('main:home')
+
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
         return super().form_valid(form)
